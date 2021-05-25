0843229deeccb641d5eafefbedc282301f6f69eb
Ajith Anand <ajithanand303@gmail.com>
2021-05-25 16:32:48 +0530

    parent

---
+++ HostelManagement/parent/urls.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from django.conf import settings
+from django.urls import path, register_converter
+from .import views
+from  django.conf.urls import (handler404)
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+
+]
+urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
