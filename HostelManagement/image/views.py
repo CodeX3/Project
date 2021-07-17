535f518ee50dd8b256aff8ca0ea40477ef658663
Shifan <shifanchippushifan@gmail.com>
2021-07-17 16:49:14 +0530

    temp

--- HostelManagement/image/views.py
+++ HostelManagement/image/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from .form import ImageForm
 from .models import Image
-from User.models import warden
-def index(request):
+from User.models import warden,student
+def admin_gallery(request):
     value = request.session.get('admin')
     user = warden.objects.get(id=value)
     if request.method == "POST":
@@ -14,7 +14,26 @@ def index(request):
             form.save()
             obj=form.instance
             print("data stored")
-            return render(request,"gallery.html",{"obj":obj,'user':user})
+            return render(request,"admin_gallery.html",{"obj":obj,'user':user})
+        else:
+            print("rejected")
+            form=ImageForm()
+    form=ImageForm()
+    img=Image.objects.all()
+    print(img)
+    return render(request,"admin_gallery.html",{"img":img,"form":form,'user':user})
+
+def student_gallery(request):
+    id = request.session.get('userid')
+    user = student.objects.get(sd_id=id)
+    if request.method == "POST":
+        print("post method")
+        form=ImageForm(data=request.POST,files=request.FILES)
+        if form.is_valid():
+            form.save()
+            obj=form.instance
+            print("data stored")
+            return render(request,"student_gallery.html",{"obj":obj,'user':user})
         else:
             print("rejected")
             form=ImageForm()
@@ -22,5 +41,5 @@ def index(request):
     form=ImageForm()
     img=Image.objects.all()
     print(img)
-    return render(request,"gallery.html",{"img":img,"form":form,'user':user})
+    return render(request,"student_gallery.html",{"img":img,"form":form,'user':user})
 
